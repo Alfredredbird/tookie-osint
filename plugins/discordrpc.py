0df6404256fe14e0fe69736b02a35293d1f22a1c
alfredredbird <105014217+Alfredredbird@users.noreply.github.com>
2025-09-02 19:52:20 -0700

    bug fix

--- plugins/discordrpc.py
+++ plugins/discordrpc.py
@@ -1,20 +1,22 @@
 from pypresence import Presence
 import time
 
+try:
+ client_id = '1280593779203637329'
+ RPC = Presence(client_id)  
+ RPC.connect()  
 
-client_id = '1280593779203637329'
-RPC = Presence(client_id)  
-RPC.connect()  
 
-
-RPC.update(
+ RPC.update(
     state="Investigating",
     details="Looking For Someone",
     start=time.time()  
-)
+ )
 
-try:
+ try:
     while True:
         time.sleep(15)  
-except KeyboardInterrupt:
+ except KeyboardInterrupt:
     RPC.close()
+except Exception:
+    print("Cant start Discord RPC :(")
